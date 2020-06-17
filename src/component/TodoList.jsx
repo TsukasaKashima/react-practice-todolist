8d7a7a40bb4e92cd2755712ba5e76007a52360b8
鹿島司 <kashimatsukasa@air.local>
2020-06-18 00:14:10 +0900

    feature/2が完了しました！

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -3,37 +3,62 @@ import { useState } from "react";
 
 export default () => {
   const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
   const [todoList, setTodolist] = useState([]);
 
   function handleChange(event) {
     const newValue = event.target.value;
     setValue(newValue);
   }
-
+  function titleChange(event) {
+    const newTitle = event.target.value;
+    setTitle(newTitle);
+  }
   function addTodo() {
-    const newTodo = { content: value };
-    const newTodoList = [todoList, newTodo];
-    //const newTodoList = todoList.concat(newTodo);でもOk
-    //todoListという配列[]に{ content: value }を入れているのが15行目のコード
+    const newTodo = { TodoTitle: title, TodoContent: value };
+    const newTodoList = [...todoList, newTodo];
     setTodolist(newTodoList);
     setValue("");
+    setTitle("");
+    textAlert();
   }
-
+  function textAlert() {
+    const textCheck01 = document.getElementById("input01").value;
+    const textCheck02 = document.getElementById("input02").value;
+    if (textCheck01 === "" || textCheck02 === "") {
+      alert("いずれかが未入力です");
+    }
+  }
   return (
     <div>
       <div className="header">
-        <input
-          type="text"
-          placeholder="enter task"
-          value={value}
-          onChange={handleChange}
-        />
-        <button onClick={addTodo}>add</button>
+        <div className="form-block">
+          <label>タイトル</label>
+          <input
+            id="input01"
+            type="text"
+            placeholder="Enter Title"
+            value={title}
+            onChange={titleChange}
+          />
+        </div>
+        <div className="form-block">
+          <label>内容</label>
+          <input
+            id="input02"
+            type="text"
+            placeholder="Enter Task"
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTodo}>Add</button>
       </div>
       {todoList.map((todo, index) => {
         return (
           <div className="list" key={index}>
-            content: {todo.content}
+            <div className="title">Title:{todo.TodoTitle}</div>
+            {todo.TodoContent}
           </div>
         );
       })}
