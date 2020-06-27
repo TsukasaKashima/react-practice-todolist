bc277fd4bad7cb6adb0a7ecae7989d4013ec283c
鹿島司 <kashimatsukasa@air.local>
2020-06-28 00:19:50 +0900

    不要な引数を削除

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
 
-let store = createStore(reducers, TodoList);
+let store = createStore(reducers);
 
 function App() {
   return (
